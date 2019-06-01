980aaac069292af6d7a05ce4c55bf41136e78a9d
Harold Ridley <harold.ridley24@gmail.com>
2019-06-01 11:02:12 -0700

    fixing async test

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -1,7 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.hridley.jokesdisplaylib.DisplayJokesActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,7 +23,14 @@ public class AsyncTaskTest {
     @Test
     public void testAsyncTask() throws InterruptedException, ExecutionException {
         // on the MainActivity execute the AsyncTask
-        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(new EndpointsAsyncTask.TaskCompleteListener() {
+            @Override
+            public void onTaskComplete(String result) {
+                Intent intent = new Intent(activityTestRule.getActivity(), DisplayJokesActivity.class);
+                intent.putExtra(DisplayJokesActivity.EXTRAS_JOKE, result);
+                activityTestRule.getActivity().startActivity(intent);
+            }
+        });
         endpointsAsyncTask.execute(activityTestRule.getActivity());
 
         // the String returned in the onPostExecute is being retrieved
--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -40,7 +41,8 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
         try {
             return myApiService.getRandomJoke().execute().getData();
         } catch (IOException e) {
-            return e.getMessage();
+            Log.i("Error Retrieving Jokes",e.getMessage());
+            return null;
         }
     }
 
